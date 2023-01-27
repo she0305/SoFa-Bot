2f95d070f5212d9f338a00c53c2c956b4efb3030
Amy Kim <amykimmy0305@gmail.com>
2023-01-26 19:33:36 -0500

    sofabot command

--- src/commands/sofabot.ts
+++ src/commands/sofabot.ts
@@ -2,12 +2,12 @@ import { Command } from "../structures/Command";
 
 export default new Command({
   name: "sofabot",
-  description: "Say Hi",
+  description: "Say Hi to Safabot",
   run: async ({ interaction }) => {
     await interaction.followUp(
-        "\"A wild SoFa bot appeared!\"\n\nHi, my name is Sofabot - I'm a bot created by a member of the Software Factory community. I'm here to help you with your onboarding and to help you find the right resources to learn more about coding. If you have any questions, feel free to ask me! You can find the list of commands by typing /help'\n\n\"SoFa bot disappeared!\" ");
+        ` "A wild SoFa bot appeared!\n\n"Hi, my name is Sofabot - I'm a bot created by a member of the Software Factory community. I'm here to help you with your onboarding and to help you find the right resources to learn more about coding. \n\nIf you have any questions, feel free to ask me! You can find the list of commands by typing /help'\n\n"SoFa bot disappeared!" `);
   },
-});
+})
 
 // import { Command } from "../../structures/Command";
 //
